c6e4d424121ed1d7f021ddd78e20a86c0f93b568
boumitran <boumitran@32studio.org>
2025-10-22 08:26:47 +0000

    remove cors

--- index.js
+++ index.js
@@ -10,18 +10,7 @@ const app = express();
 const firestore = new Firestore();
 const port = process.env.PORT || 8080;
 
-const whitelist = ['https://32studio.org', 'https://run.app', 'https://cloudworkstations.dev']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 const openapiSpec = fs.readFileSync('openapi.yaml', 'utf8');
