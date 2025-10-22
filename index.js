cc641faa69183b31dc9d79c66649c7bb2e62556e
boumitran <boumitran@32studio.org>
2025-10-22 08:09:13 +0000

    domain whitelist

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const slugify = require('slugify');
 const path = require('path');
 const fs = require('fs');
 const Joi = require('joi'); // Import Joi
+const cors = require('cors');
 
 const app = express();
 const firestore = new Firestore();
 const port = process.env.PORT || 8080;
 
+const whitelist = ['https://32studio.org', 'https://run.app', 'https://cloudworkstations.dev']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const openapiSpec = fs.readFileSync('openapi.yaml', 'utf8');
